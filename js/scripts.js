9118c39b517904610ed976404a97c08b98088dab
evagrean <54578535+evagrean@users.noreply.github.com>
2019-09-25 13:21:00 +0200

    Added functions and return object that don't require jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,31 @@ var pokemonRepository = (function(){
 
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';  // link for loading pokeapi
 
+  function getAll() {
+    return repository;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object') {
+      repository.push(pokemon);
+    }
+    else {
+      console.log('only objects with predetermined keys can be added!');
+    }
+  }
+
+  // addListItem function written by exchanging querySelector with jQuery
 
 
-
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
+  };
 
 })();
